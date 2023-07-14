c8ae04cfe7555344b288af21b8a09d4c73c8ffc8
AyoubHub212 <ayoubelouarzazi1@gmail.com>
2023-07-15 00:08:37 +0100

    ;

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using putchar to print alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 122)
+	{
+		putchar (i);
+		i++;
+	}
+	return (0);
+}

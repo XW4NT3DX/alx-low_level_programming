fe9e7f3cadf4233f90d7cb63cc156e2574bcb30d
AyoubHub212 <ayoubelouarzazi1@gmail.com>
2023-07-15 02:36:58 +0100

    ;

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using putchar to print alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i == 57)
+			break;
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

80a366f61d4e2452b9220198b02cdecb6a3a82af
AyoubHub212 <ayoubelouarzazi1@gmail.com>
2023-07-24 17:43:33 +0100

    ;

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include"MAIN_H"
+/**
+ *swap_int- swapping values of two integers
+ *@a: int
+ *@b: int
+ *Return : 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}

bc3b99d06fbe59e77c7f4150f343a75816918d18
AyoubHub212 <ayoubelouarzazi1@gmail.com>
2023-07-25 01:28:38 +0100

    ;

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ *rev_string- description
+ *@s: int
+ *Return: 0
+ */
+void rev_string(char *s)
+{
+	int *p = &s;
+	int a;
+
+	while (*s)
+	{
+		s++;
+	}
+	while (p < s)
+	{
+		*s = a;
+		*p = s;
+		*p = a;
+		p++;
+		s--;
+	}
+
+}
+
